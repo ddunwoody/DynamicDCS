da9bdffec14e5bb52bf8405e1f60a85ade5c69cc
Andrew Finegan <andrew.finegan@gmail.com>
2019-08-12 02:16:47 -0400

    removing console logs

--- controllers/events/frontend/S_EVENT_TAKEOFF.js
+++ controllers/events/frontend/S_EVENT_TAKEOFF.js
@@ -37,12 +37,12 @@ _.set(exports, 'processEventTakeoff', function (serverName, sessionName, eventOb
 					}
 					if (curIUnit) {
 						iPlayer = _.find(playerArray, {name: _.get(curIUnit, 'playername')});
-						// console.log('takeoff: ', _.get(curIUnit, 'playername'));
+						console.log('takeoff: ', _.get(curIUnit, 'playername'));
 						if (_.get(iPlayer, 'ucid')) {
 							if (weaponComplianceController.checkWeaponComplianceOnTakeoff(serverName, iPlayer, curIUnit)) {
 								proximityController.getBasesInProximity(serverName, _.get(curIUnit, 'lonLatLoc'), 5, curUnitSide)
 									.then(function(friendlyBases) {
-										console.log('T6', friendlyBases);
+										// console.log('T6', friendlyBases);
 										if(friendlyBases.length > 0) {
 											iCurObj = {
 												sessionName: sessionName,
@@ -53,10 +53,12 @@ _.set(exports, 'processEventTakeoff', function (serverName, sessionName, eventOb
 												roleCode: 'I',
 												msg: 'C: '+ _.get(curIUnit, 'type') + '('+_.get(curIUnit, 'playername')+') has taken off' + place
 											};
+											/*
 											console.log('T7', serverName,
 												iPlayer,
 												curIUnit,
 												'Takeoff');
+											 */
 											userLivesController.removeLifePoints(
 												serverName,
 												iPlayer,
